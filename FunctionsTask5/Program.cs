a65f2e4fc3513a68f39e580de20dd2d0be218a32
Corvette31 <corvette.ss@yandex.ru>
2022-07-04 10:23:46 +0300

    исправления

--- FunctionsTask5/Program.cs
+++ FunctionsTask5/Program.cs
@@ -7,27 +7,26 @@ namespace FunctionsTask5
         static void Main(string[] args)
         {
             Random random = new Random();
-            int minValue = 0;
             int maxValue = 101;
             int arraySize = 10;
             int[] array = new int[arraySize];
 
-            FillArray(ref array, random, minValue, maxValue);
+            FillArray(array, random,  maxValue);
 
             Console.WriteLine("Массив: ");
             ShowArray(array);
 
-            Shuffle(ref array, random, minValue);
+            Shuffle(array, random);
 
             Console.WriteLine("Массив со случайно перемещенными элементами");
             ShowArray(array);
         }
 
-        static void FillArray(ref int[] array, Random random, int minValue, int maxValue)
+        static void FillArray(int[] array, Random random, int maxValue)
         {
             for (int i = 0; i < array.Length; i++)
             {
-                array[i] = random.Next(minValue, maxValue);
+                array[i] = random.Next(maxValue);
             }
         }
 
@@ -41,28 +40,15 @@ namespace FunctionsTask5
             Console.WriteLine();
         }
 
-        static void Shuffle(ref int[] array, Random random, int minValue)
+        static void Shuffle(int[] array, Random random)
         {
             for (int i = 0; i < array.Length - 1; i++)
             {
-                int newPosition = GetRandomPosition(i, random, minValue, array.Length);
+                int newPosition = random.Next(array.Length);
                 int temp = array[i];
                 array[i] = array[newPosition];
                 array[newPosition] = temp;
             }
         }
-
-        static int GetRandomPosition(int index, Random random, int minValue, int maxValue)
-        {
-            int newPosition = index;
-
-            while (newPosition == index)
-            {
-                newPosition = random.Next(minValue, maxValue);
-            }
-
-            return newPosition; 
-        }
-
     }
 }
